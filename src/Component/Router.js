0d7d359fb0dcba291f492177a5c3c0ee40dfce51
FEDAkila <akilamurugesan200311@gmail.com>
2023-11-17 20:55:40 -0330

    add

--- src/Component/Router.js
+++ src/Component/Router.js
@@ -10,7 +10,7 @@ import {Details} from './Details'
 
 export const Router=()=>{
     return(
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
             <Route path="/" element={<Home/>}>   </Route>
             <Route path="/Details"  element={<Details/>}></Route>
