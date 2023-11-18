c9f79c8220b6cf40eb90c73ba4e71a3a032eb16f
FEDAkila <akilamurugesan200311@gmail.com>
2023-11-18 11:47:05 -0330

    add

--- src/Page/Details.js
+++ src/Page/Details.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
+import './Details.scss'
 
 export const Details=()=>{
     const [param]=useSearchParams()
@@ -33,12 +34,28 @@ export const Details=()=>{
         <div>
             {update.map((v,i)=>{
                 return(
-                    <div style={{textAlign:"center"}}>
-                       <h3>{v.name}</h3> 
+                    <section>
+                        <div style={{width:"90%",margin:"0 auto",padding:"10px 0"}}>
+                    <div style={{display:"flex",justifyContent:"center",alignItems:"center"}} className="ch" >
+                        <div style={{padding:"10px"}} className="ali">
                        <img src={v.image.original}></img>
+                       </div>
+                       <div style={{padding:"10px"}} className="ali">
+                       <p><h3 style={{display:"inline-block"}}>Movie Name:</h3> {v.name}</p> 
+                       <p><h3 style={{display:"inline-block"}}>Movie Time:</h3> {v.airtime}</p>
+                       <p><h3 style={{display:"inline-block"}}>Movie Date:</h3> {v.airdate}</p>
+                        <h3 style={{paddingBottom:"5px"}}>Summary:</h3>
+                        <p style={{paddingBottom:"20px"}} >{v.summary}</p>
+                        <span style={{fontSize:"18px",fontWeight:"700"}}>Reference Link: </span>
+                        <a style={{textDecoration:"none",color:"blue"}} href={v.url} target="_blank">Link</a>
+                        </div>
+
                     </div>
+                    </div>
+                    </section>
                 )
             })}
         </div>
+
     )
 }
